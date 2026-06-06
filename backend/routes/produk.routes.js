5c7b1416cd6651cf39d5b0491032b02dbb118273
ELGHIFARY111 <elgifaryamink@gmail.com>
2026-06-06 15:24:19 +0700

    update

--- backend/routes/produk.routes.js
+++ backend/routes/produk.routes.js
@@ -3,19 +3,7 @@ const router = express.Router();
 const produkController = require('../controllers/produk.controller');
 const { authenticateToken } = require('../middleware/auth.middleware');
 
-const multer = require('multer');
-const path = require('path');
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'public/uploads/');
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, uniqueSuffix + path.extname(file.originalname));
-    }
-});
-const upload = multer({ storage: storage });
+const upload = require('../middleware/upload.middleware');
 
 
 router.get('/', produkController.getAllProducts);
